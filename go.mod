29474a21c540f397344f1e6ba98fa523b7b05255
Arian94 <arianpourarian@gmail.com>
2021-08-13 21:07:03 +0430

    added users, jwt and restructured files

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/araddon/dateparse v0.0.0-20210429162001-6b43995a97de // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gin-gonic/gin v1.7.2
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/itsjamie/gin-cors v0.0.0-20160420130702-97b4a9da7933
--- main.go
+++ main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"time"
 
-	hotel "Resort/src"
+	hotel "Resort/src/hotel"
+	runMysqlDatabase "Resort/src/mysql-database"
+	signupLogin "Resort/src/signup-login"
 
 	"github.com/gin-gonic/gin"
 	cors "github.com/itsjamie/gin-cors"
 )
 
+// var Db *sql.DB
+
 func main() {
+	// runDatabase()
+	runMysqlDatabase.RunDatabase()
+
 	router := gin.Default()
 
 	router.Use(cors.Middleware(cors.Config{
@@ -22,9 +29,33 @@ func main() {
 		ValidateHeaders: false,
 	}))
 
+	router.POST("/signup", signupLogin.Signup)
 	router.POST("/hotelRooms", hotel.CheckRoomAvailability)
-	// router.GET("/alaki", alaki)
-	// router.POST("/albums", postAlbums)
+	// router.POST("/reserveRoom", hotel.ReserveRoom)
 
 	router.Run("localhost:8080")
 }
+
+// func runDatabase() {
+// 	// Capture connection properties.
+// 	cfg := mysql.Config{
+// 		User:   "arian",
+// 		Passwd: "123",
+// 		Net:    "tcp",
+// 		Addr:   "127.0.0.1:3306",
+// 		DBName: "resort",
+// 	}
+
+// 	// Get a database handle.
+// 	var err error
+// 	Db, err = sql.Open("mysql", cfg.FormatDSN())
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	pingErr := Db.Ping()
+// 	if pingErr != nil {
+// 		log.Fatal(pingErr)
+// 	}
+// 	log.Println("Connected!")
+// }
